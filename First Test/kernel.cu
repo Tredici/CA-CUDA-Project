3ca19a3b6b4d8594e7f79df939822aeea9a9c21e
Tredici <marcolucio1999@gmail.com>
2022-05-31 01:57:00 +0200

    First (bugged) compiling version

--- First Test/kernel.cu
+++ First Test/kernel.cu
@@ -2,11 +2,90 @@
 #include "cuda_runtime.h"
 #include "device_launch_parameters.h"
 
+#include "modules/CPP-csv-parser/csv.hh"
+#include "modules/CPP-math-utils/convertions.hh"
+
 #include <stdio.h>
+#include <istream>
+#include <fstream>
+#include <string>
+#include <utility>
+#include <optional>
 
 #include <thrust/device_vector.h>
+#include <thrust/host_vector.h>
 
-cudaError_t addWithCuda(int *c, const int *a, const int *b, unsigned int size);
+using namespace std;
+
+using data_type = double;
+
+// Macro can be used anywhere, regardeless of CUDA support for C++ code
+#define COUPLE_NUMBER(n) ((n-1)*(n)/2)
+
+// MIN macro
+#define MIN(x,y) ((x) < (y) ? (x) : (y))
+
+[[noreturn]] void help(const std::string& cmd, int exit_status = EXIT_FAILURE) {
+    std::cerr << "Usage:\n";
+    std::cerr << '\t' << cmd << " input.csv\n";
+    std::cerr << "\t\t" << "input.csv: file containing time series\n";
+    exit(exit_status);
+}
+
+struct Pair {
+    int first = 0, second = 0;
+};
+
+struct PCC_Partial {
+    long long count{};
+    double sum_1{};
+    double sum_2{};
+    double sum_1_squared{};
+    double sum_2_squared{};
+    double sum_prod{};
+};
+
+
+// Cuda seems not supporting classes, this function was the core
+// of the math::sets::couple class so I extracted it to
+//  https://github.com/Tredici/CPP-math-utils/blob/b3b3f844d51b1014e20d329009461b8ac74ef21d/couple.hh#L18
+__device__ Pair pair(int n, int i) {
+    // candidate supposing al pairs are ok
+    Pair p{ i / n, i % n };
+
+    // first column and no overflow?
+    if (p.first == 0 && p.second + 1 < n) {
+        p.second += 1;
+        return p;
+    }
+    else if (p.first == 0 && p.second + 1 == n) {
+        p.first = 1; p.second = 2;
+        return p;
+    }
+
+    // reduce problem with recursion
+    // [0,1] for new base will be
+    // translated to [p[0], p[0]+1]
+    Pair base{ p.first, p.first };
+    // all points in the triangle
+    // marked by [p[0], p[0]]
+    // must be ignored, others must be
+    // counted
+    auto remaining = i - ((n - 1) * p.first - (p.first - 1) * p.first / 2);
+    auto p2 = ::pair(n - p.first, remaining);
+    p2.first += base.first, p2.second += base.second;
+    return p2;
+}
+
+__device__ Pair inc(int n, Pair couple) {
+    if (++couple.second == n) {
+        couple.first += 1;
+        couple.second = couple.first + 1;
+        return couple;
+    }
+    return couple;
+}
+
 
 __global__ void addKernel(int *c, const int *a, const int *b)
 {
@@ -14,110 +93,173 @@ __global__ void addKernel(int *c, const int *a, const int *b)
     c[i] = a[i] + b[i];
 }
 
-int main()
+// Return a vector that will hold partial results
+auto allocate_partial_container(int ts_count) {
+    return thrust::device_vector<PCC_Partial>(COUPLE_NUMBER(ts_count));
+}
+
+auto allocate_result_container(int ts_count) {
+    return thrust::device_vector<data_type>(COUPLE_NUMBER(ts_count));
+}
+
+__device__ void operator+=(PCC_Partial& p1, const PCC_Partial& p2) {
+    p1.count += p2.count;
+    p1.sum_1 += p2.sum_1;
+    p1.sum_2 += p2.sum_2;
+    p1.sum_1_squared += p2.sum_1_squared;
+    p1.sum_2_squared += p2.sum_2_squared;
+    p1.sum_prod += p2.sum_prod;
+}
+
+// comput partial pcc on two time series
+__device__ void calculate_pcc(PCC_Partial* partial, const data_type* v1, const data_type* v2, int length) {
+    PCC_Partial ans;
+    for (int i{}; i != length; ++i) {
+        const auto v_1 = v1[i];
+        const auto v_2 = v2[i];
+        ans.sum_1 += v_1;
+        ans.sum_2 += v_2;
+        ans.sum_1_squared += v_1 * v_1;
+        ans.sum_2_squared += v_2 * v_2;
+        ans.sum_prod += v_1 * v_2;
+    }
+    ans.count = length;
+    *partial += ans;
+}
+
+__global__ void evaluate(PCC_Partial* partial, data_type** chunk, int length) {
+    int limit = COUPLE_NUMBER(length);
+    // columns per thread
+    auto cpt = limit / blockDim.x;
+    // more thread than items? Might happet if columns are too few
+    if (cpt == 0) {
+        if (threadIdx.x < limit) {
+            auto i = threadIdx.x;
+            auto couple = ::pair(length, i);
+            calculate_pcc(&partial[i], chunk[couple.first], chunk[couple.second], length);
+        }
+    }
+    // else at least one columnt per thread
+    else {
+        // for each pair assigned to this thread
+        auto beginning = cpt * blockDim.x;
+        auto end = MIN(beginning + cpt, limit);
+        auto couple = ::pair(length, beginning);
+        while (beginning != end) {
+            calculate_pcc(&partial[beginning], chunk[couple.first], chunk[couple.second], length);
+            // next pair
+            couple = ::inc(length, couple);
+            ++beginning;
+        }
+    }
+}
+
+__device__ data_type compute(const PCC_Partial& pcc) {
+    if (pcc.count == 0) {
+        return 0;
+    }
+    const auto num = (pcc.sum_prod - (pcc.sum_1 * pcc.sum_2) / pcc.count);
+    const auto den = (pcc.sum_1_squared - (pcc.sum_1 * pcc.sum_1 / pcc.count)) * (pcc.sum_2_squared - (pcc.sum_2 * pcc.sum_2 / pcc.count));
+    // check for div by 0
+    return den ? num / std::sqrt(den) : 0;
+}
+
+// calculate results element by element
+__global__ void compute_results(int n, data_type* res, const PCC_Partial* partials) {
+    int limit = COUPLE_NUMBER(n);
+    // columns per thread
+    auto cpt = limit / blockDim.x;
+    // more thread than items? Might happet if columns are too few
+    if (cpt == 0) {
+        auto i = threadIdx.x;
+        res[i] = compute(partials[i]);
+    }
+    // else at least one columnt per thread
+    else {
+        // for each pair assigned to this thread
+        const auto beginning = cpt * blockDim.x;
+        const auto end = MIN(beginning + cpt, limit);
+        // compute final result
+        for (int i = beginning; i != end; ++i) {
+            res[i] = compute(partials[i]);
+        }
+    }
+}
+
+void print_results(const int n, const thrust::host_vector<data_type>& hres) {
+    int p = 0;
+    for (int i{}; i != n-1; ++i) {
+        for (int j{ i + 1 }; j != n; ++j) {
+            std::cout << "(" << i << "," << j << ") " << hres[p++] << '\n';
+        }
+    }
+}
+
+std::optional<std::vector<thrust::device_vector<data_type>>> get_chunk(csv::reader& r, std::size_t line_count) {
+    std::vector<thrust::host_vector<data_type>> tmp(r.column_count());
+    for (auto& v : tmp) {
+        v.reserve(line_count);
+    }
+    auto counter = line_count;
+    while (r.can_read() && counter--) {
+        auto line = r.getline();
+        for (size_t i = 0; i < line.size(); ++i) {
+            tmp[i].push_back(math::convertions::ston<data_type>(line.data()[i]));
+        }
+    }
+    if (counter == line_count) {
+        return std::nullopt;
+    }
+    // many columns as tmp
+    std::vector<thrust::device_vector<data_type>> ans;
+    ans.reserve(tmp.size());
+    for (const auto& v : tmp) {
+        ans.emplace_back(v);
+    }
+    return ans;
+}
+
+
+int main(int argc, char const* argv[])
 {
-    const int arraySize = 5;
-    const int a[arraySize] = { 1, 2, 3, 4, 5 };
-    const int b[arraySize] = { 10, 20, 30, 40, 50 };
-    int c[arraySize] = { 0 };
+    std::size_t chunk_size = 1000;
 
-    // Add vectors in parallel.
-    cudaError_t cudaStatus = addWithCuda(c, a, b, arraySize);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "addWithCuda failed!");
-        return 1;
+    std::string input("test-dataset.csv");
+    std::ifstream fin(input);
+    if (!fin.good()) {
+        std::cerr << "Failed to open file '" << input << "'\n";
+        help(argv[0], EXIT_FAILURE);
     }
 
-    printf("{1,2,3,4,5} + {10,20,30,40,50} = {%d,%d,%d,%d,%d}\n",
-        c[0], c[1], c[2], c[3], c[4]);
+    csv::reader r(fin);
+    auto ts_count = r.column_count();
 
-    // cudaDeviceReset must be called before exiting in order for profiling and
-    // tracing tools such as Nsight and Visual Profiler to show complete traces.
-    cudaStatus = cudaDeviceReset();
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaDeviceReset failed!");
-        return 1;
+    // since this allocation the map containing
+    // the results will never change its size,
+    // so it is safe to access distinct elements
+    // from different thread because no ops will
+    // be performed on its structure
+    auto partial = allocate_partial_container(ts_count);
+
+    while (true) {
+        auto chunk = get_chunk(r, chunk_size);
+        if (!chunk.has_value()) {
+            break;
+        }
+        thrust::device_vector<data_type*> c;
+        c.reserve(chunk.value().size());
+        for (auto& v : chunk.value()) {
+            c.push_back(thrust::raw_pointer_cast(&v[0]));
+        }
+        evaluate <<< 1, 1 >>> (thrust::raw_pointer_cast(&partial[0]), thrust::raw_pointer_cast(&c[0]), ts_count);
     }
+
+    auto res = allocate_result_container(ts_count);
+    compute_results <<< 1, 1 >>> (ts_count, thrust::raw_pointer_cast(&res[0]), thrust::raw_pointer_cast(&partial[0]));
+    thrust::host_vector<data_type> hres(res);
+    print_results((int)ts_count, hres);
 
     return 0;
 }
 
-// Helper function for using CUDA to add vectors in parallel.
-cudaError_t addWithCuda(int *c, const int *a, const int *b, unsigned int size)
-{
-    int *dev_a = 0;
-    int *dev_b = 0;
-    int *dev_c = 0;
-    cudaError_t cudaStatus;
 
-    // Choose which GPU to run on, change this on a multi-GPU system.
-    cudaStatus = cudaSetDevice(0);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaSetDevice failed!  Do you have a CUDA-capable GPU installed?");
-        goto Error;
-    }
-
-    // Allocate GPU buffers for three vectors (two input, one output)    .
-    cudaStatus = cudaMalloc((void**)&dev_c, size * sizeof(int));
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMalloc failed!");
-        goto Error;
-    }
-
-    cudaStatus = cudaMalloc((void**)&dev_a, size * sizeof(int));
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMalloc failed!");
-        goto Error;
-    }
-
-    cudaStatus = cudaMalloc((void**)&dev_b, size * sizeof(int));
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMalloc failed!");
-        goto Error;
-    }
-
-    // Copy input vectors from host memory to GPU buffers.
-    cudaStatus = cudaMemcpy(dev_a, a, size * sizeof(int), cudaMemcpyHostToDevice);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMemcpy failed!");
-        goto Error;
-    }
-
-    cudaStatus = cudaMemcpy(dev_b, b, size * sizeof(int), cudaMemcpyHostToDevice);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMemcpy failed!");
-        goto Error;
-    }
-
-    // Launch a kernel on the GPU with one thread for each element.
-    addKernel<<<1, size>>>(dev_c, dev_a, dev_b);
-
-    // Check for any errors launching the kernel
-    cudaStatus = cudaGetLastError();
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "addKernel launch failed: %s\n", cudaGetErrorString(cudaStatus));
-        goto Error;
-    }
-    
-    // cudaDeviceSynchronize waits for the kernel to finish, and returns
-    // any errors encountered during the launch.
-    cudaStatus = cudaDeviceSynchronize();
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaDeviceSynchronize returned error code %d after launching addKernel!\n", cudaStatus);
-        goto Error;
-    }
-
-    // Copy output vector from GPU buffer to host memory.
-    cudaStatus = cudaMemcpy(c, dev_c, size * sizeof(int), cudaMemcpyDeviceToHost);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMemcpy failed!");
-        goto Error;
-    }
-
-Error:
-    cudaFree(dev_c);
-    cudaFree(dev_a);
-    cudaFree(dev_b);
-    
-    return cudaStatus;
-}
